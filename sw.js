0ccd992e423b0ec2219402478aee9c3b933d4626
AYAD_MOUNIR <contact.ayad.mounir@gmail.com>
2026-05-30 20:10:33 +0100

    feat: update sw for pwa

--- sw.js
+++ sw.js
@@ -1,4 +1,19 @@
+const CACHE_NAME = 'age-calc-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/script.js',
+    '/style.css',
+    '/icon.svg',
+    '/manifest.json'
+];
+
 self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(ASSETS);
+        })
+    );
     self.skipWaiting();
 });
 
@@ -7,5 +22,9 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-    e.respondWith(fetch(e.request));
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
 });
